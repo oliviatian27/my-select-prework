5c7a59dc574c2033491b602ef39d0a9c14ae6795
oliviatian27@gmail.com <oliviatian27@gmail.com>
2018-07-23 14:13:42 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+   i=0
+   result=[]
+   while i<collection.length
+      if yield(collection[i])==true
+        result << collection[i]
+      end
+      i+=1
+   end
+   return result
 end
